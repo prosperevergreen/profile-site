ab04521d11d0d8717cef18002d40bedbb9876cbd
Prosper Evergreen <prosperevergreen@gmail.com>
2022-07-25 11:21:22 +0300

    deploy

---
+++ js/profile.5068d2ce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["profile"],{c66d:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("Coming soon...")])])}],l=t("2877"),o={},r=Object(l["a"])(o,s,c,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=profile.5068d2ce.js.map
